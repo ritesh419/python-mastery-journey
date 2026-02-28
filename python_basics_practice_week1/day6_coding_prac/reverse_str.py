81dfe26150f7092eac24561d12b8e6af38a1bd08
Ritesh Garkoti <singhharshit419gmail.com>
2026-02-28 23:18:32 +0530

    day 6 first problem solved

---
+++ python_basics_practice_week1/day6_coding_prac/reverse_str.py
@@ -0,0 +1,14 @@
+def reverse_string(s):      # building new str
+    emp_str = "" 
+    for char in s:
+        emp_str = char + emp_str
+    return emp_str
+
+def rev_str_list(s):        # using indexed loop  
+    emp_str = ""
+    for i in range(len(s)-1, -1, -1):
+        emp_str += s[i]
+    return emp_str
+
+print(reverse_string("ritesh"))
+print(rev_str_list("ayush"))
